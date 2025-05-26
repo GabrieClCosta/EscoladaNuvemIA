65e3b5aabcd0854f82887465392df296b0d6cd8c
Gabriel Luis <gabrieclcosta@gmail.com>
2025-05-25 23:00:07 -0300

    Add a new Python script to calculate the difference between two numbers

---
+++ AtividadePratica01/calcula_diferenca.py
@@ -0,0 +1,9 @@
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+D = int(input("Digite o valor de D: "))
+
+diferenca = (A * B) - (C * D)
+
+print("A formula para calcular a diferença é: (A * B) - (C * D)")
+print(f"A diferença entre o produto de {A} e {B} e o produto de {C} e {D} é {diferenca}.")
