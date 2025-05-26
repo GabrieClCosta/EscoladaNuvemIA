aa78821442e21a5780c49cf4aae1f67d6f4de27b
Gabriel Luis <gabrieclcosta@gmail.com>
2025-05-25 22:48:55 -0300

    AtividadePratica01

---
+++ AtividadePratica01/preco_total.py
@@ -0,0 +1,4 @@
+preco_unitario = 12.40
+quantidade = 3
+preco_total = preco_unitario * quantidade
+print(f"O preço total para {quantidade} cadeiras infantis a R${preco_unitario:.2f} cada é R${preco_total:.2f}.")
